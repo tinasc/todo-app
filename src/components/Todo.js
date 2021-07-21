59433c4330b87a06eb06730b99e9b2511812ba0a
Tina <tinasylviac@gmail.com>
2021-07-21 16:34:53 -0400

    Deleting Todos

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,20 @@
 import React from 'react'
 
-const Todo = ({text}) => {
+const Todo = ({text, todos, todo, setTodos}) => {
+
+    const deleteHandler = () => {
+        setTodos(todos.filter((el) => el.id !== todo.id));
+    };
     return (
         <div className="todo">
-            <li clasName="todo-item">{text}</li>
-            <button className="complete-btn"><i className="fas fa-check"></i></button>
-            <button className="trash-btn"><i className="fas fa-trash"></i></button>
+            <li className="todo-item">{text}</li>
+            <button className="complete-btn">
+                <i className="fas fa-check"></i>
+            </button>
+            <button onClick={deleteHandler} className="trash-btn">
+                <i className="fas fa-trash"></i>
+            </button>
         </div>
-    )
-}
+    );
+};
 export default Todo;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import Todo from './Todo';
 
-const TodoList = ({todos}) => {
+const TodoList = ({todos,setTodos}) => {
     return (
         <div className="todo-container">
             <ul className="todo-list">
                 {todos.map((todo) =>(
-                    <Todo text={todo.text}/>
-            ))}
+                    <Todo 
+                    key={todo.id} 
+                    text={todo.text} 
+                    todo={todo}
+                    todos={todos} 
+                    setTodos={setTodos}
+                    />
+                 ))}
             </ul>
       </div>
     );
