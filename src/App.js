f2f874c6808bbcf6ab4234200e86ae56462d18c2
Tina <tinasylviac@gmail.com>
2021-07-21 23:09:32 -0400

    Saving to Local Storage

--- src/App.js
+++ src/App.js
@@ -10,8 +10,13 @@ const App = () => {
   const [status, setStatus] = useState('all');
   const [filteredTodos, setFilteredTodos] = useState([]);
 
+  useEffect(() => {    
+    getLocalTodos();
+  }, []);
+
   useEffect(() => {
     filterHandler();
+    saveLocalTodos();
   }, [todos, status]);
 
   const filterHandler = () => {
@@ -24,13 +29,25 @@ const App = () => {
         setFilteredTodos(todos.filter(todo => todo.completed === false));
         break;
 
-        default:
-          setFilteredTodos(todos);
-          break;
+      default:
+        setFilteredTodos(todos);
+        break;
     } 
-   
-  }
-  
+  };
+
+  const saveLocalTodos = () => {
+      localStorage.setItem('todos', JSON.stringify(todos));
+  };
+
+  const getLocalTodos = () => {
+    if (localStorage.getItem('todos') === null) {
+      localStorage.setItem('todos', JSON.stringify([]));
+    } else {
+      let todoLocal = JSON.parse(localStorage.getItem('todos'))
+      setTodos(todoLocal);
+    }
+  };
+
   return (
     <div className="App">
       <header>
